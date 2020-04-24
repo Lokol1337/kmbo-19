f7abda688b7ba8fb5ed030de63860af1184edcf8
Lokol1337 <58722409+Lokol1337@users.noreply.github.com>
2020-04-24 18:17:38 +0300

    Update llist.cpp

--- llist.cpp
+++ llist.cpp
@@ -256,15 +256,3 @@ LList::LList(LList&& copyLList)
 }
 
 
-int main()
-    {
-       LList a;
-       // LList c;
-        //for(int i = 0;i < 10000;i++)
-		//c.push_back(i);
-       	for(int i = 0;i < 10000;i++)
-		a.push_back(i);
-
-        LList b(move(a));
-        //LList d(c);
-    }
