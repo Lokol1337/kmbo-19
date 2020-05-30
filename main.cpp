be72d9709fe8b45b80076126e271c121dc8ea648
Lokol1337 <58722409+Lokol1337@users.noreply.github.com>
2020-05-30 09:23:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,20 +3,15 @@
 
 using namespace std;
 
-void dump(const LList& l)
-{
-	for(size_t i = 0; i < l.size(); ++i, cout << " ") cout << l[i];
-}
-
 int main()
 {
-	LList a;
-       // LList c;
+	LList<int> a;
+       // LList<int> c;
         //for(int i = 0;i < 10000;i++)
 		//c.push_back(i);
        	for(int i = 0;i < 10000;i++)
 		a.push_back(i);
-        LList b(move(a));
+        LList<int> b(move(a));
         //LList d(c);
 
 }
