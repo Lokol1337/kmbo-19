2d78ae623a00cba047c126c3928f2d82062b37bd
Lokol1337 <58722409+Lokol1337@users.noreply.github.com>
2020-05-30 09:57:19 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main()
        // LList<int> c;
         //for(int i = 0;i < 10000;i++)
 		//c.push_back(i);
-       	for(int i = 0;i < 10000;i++)
-		a.push_back(i);
         LList<int> b(move(a));
+	for(int i = 0;i < 10000;i++)
+		b.push_back(i);
         //LList d(c);
 
 }
